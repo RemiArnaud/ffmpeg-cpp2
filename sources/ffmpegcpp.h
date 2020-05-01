197094073c84b06c3aa2b1a543a2850bc2b74813
erba <eric.bachard@free.fr>
2020-05-01 13:26:29 +0200

    Fix the find the headers issue

--- sources/ffmpegcpp.h
+++ sources/ffmpegcpp.h
@@ -1,5 +1,5 @@
 
-#include "ffmpeg-cpp/ffmpeg.h"
+#include "ffmpeg.h"
 
 #include "Muxing/Muxer.h"
 
@@ -11,22 +11,22 @@
 #include "Sources/RawVideoDataSource.h"
 #include "Sources/EncodedFileSource.h"
 
-#include "ffmpeg-cpp/Frame Sinks/VideoEncoder.h"
-#include "ffmpeg-cpp/Frame Sinks/AudioEncoder.h"
-#include "ffmpeg-cpp/Frame Sinks/Filter.h"
+#include "FrameSinks/VideoEncoder.h"
+#include "FrameSinks/AudioEncoder.h"
+#include "FrameSinks/Filter.h"
 
-#include "ffmpeg-cpp/Codecs/AudioCodec.h"
+#include "Codecs/AudioCodec.h"
 
-#include "ffmpeg-cpp/codecs/H265NVEncCodec.h"
-#include "ffmpeg-cpp/codecs/H264NVEncCodec.h"
-#include "ffmpeg-cpp/Codecs/VP9Codec.h"
-#include "ffmpeg-cpp/Codecs/PNGCodec.h"
-#include "ffmpeg-cpp/Codecs/JPGCodec.h"
-#include "ffmpeg-cpp/Codecs/MJPEGCodec.h"
-#include "ffmpeg-cpp/Codecs/H264Codec.h"
-#include "ffmpeg-cpp/Codecs/MPEG4Codec.h"
-#include "ffmpeg-cpp/Codecs/VideoCodec.h"
-#include "ffmpeg-cpp/Codecs/Codec.h"
+#include "codecs/H265NVEncCodec.h"
+#include "codecs/H264NVEncCodec.h"
+#include "Codecs/VP9Codec.h"
+#include "Codecs/PNGCodec.h"
+#include "Codecs/JPGCodec.h"
+#include "Codecs/MJPEGCodec.h"
+#include "Codecs/H264Codec.h"
+#include "Codecs/MPEG4Codec.h"
+#include "Codecs/VideoCodec.h"
+#include "Codecs/Codec.h"
 
 #include "FFmpegException.h"
 
