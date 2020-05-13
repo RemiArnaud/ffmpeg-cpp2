246866c02830fbd170be11ce196e89db3183f101
erba <eric.bachard@free.fr>
2020-05-14 00:49:48 +0200

    Sound and video, but needs more work

--- demos/webcam_to_video/webcam_stream_to_MKV.cpp
+++ demos/webcam_to_video/webcam_stream_to_MKV.cpp
@@ -14,13 +14,17 @@ using namespace ffmpegcpp;
 using std::string;
 using std::cerr;
 
+//#ifdef ALSA_BUFFER_SIZE_MAX
+#undef ALSA_BUFFER_SIZE_MAX
+#define ALSA_BUFFER_SIZE_MAX  524288
+
+
 void record_MKV()
 {
-    avdevice_register_all();
-
     // These are example video and audio sources used below.
+#ifndef VIDEO_MIX
     const char* videoDevice = "/dev/video0";
-
+#endif
     //  OUTPUT CODEC, linked to the encoder ...
     H264Codec  * vcodec = new H264Codec();
     vcodec->SetGenericOption("b", "2M");
@@ -28,20 +32,36 @@ void record_MKV()
     vcodec->SetGenericOption("movflags", "+faststart");
     vcodec->SetGenericOption("qmin", "12");
     vcodec->SetGenericOption("qmax", "34");
+    vcodec->SetGenericOption("async", "1");
+    vcodec->SetGenericOption("vsync", "1");
+    vcodec->SetGenericOption("threads", "4");
+
+    // ffmpeg -i input -c:v libx264 -b:v 1M -maxrate 1M -bufsize 2M -pass 2 output.mp4
 
     // FIXME : needs more tests
-    //vcodec->SetGenericOption("vbv-maxrate", "4M");
-    //vcodec->SetGenericOption("vbv-bufsize", "2M");
+    //vcodec->SetGenericOption("pass", "2");
+    vcodec->SetGenericOption("maxrate", "8M"); // or 4M
+    vcodec->SetGenericOption("bufsize", "4M"); //    2M
 
     vcodec->SetProfile("high10"); // baseline, main, high, high10, high422
     vcodec->SetTune("film");  // film animation grain stillimage psnr ssim fastdecode zerolatency
-    vcodec->SetPreset("veryslow"); // fast, medium, slow slower, veryslow placebo
+    vcodec->SetPreset("medium"); // fast, medium, slow slower, veryslow placebo
 
     vcodec->SetCrf(23);
 
-    // Create a muxer that will output the video as MKV.
-    Muxer* muxer = new Muxer("../videos/output_H264.mkv");
+    //const char* audioDevice = "hw:1,0"; // first webcam   1 is DEV alsa parameter, 0 is 
+    const char* audioDevice = "default";
 
+    const char * audioDeviceFormat = "alsa";
+    int audioSampleRate = 44100;
+    int audioChannels   = 2;
+
+    AudioCodec * audioCodec = new AudioCodec(AV_CODEC_ID_AAC);
+
+    //audioCodec->Open(64000, AV_SAMPLE_FMT_FLTP, audioSampleRate);
+
+    // Create a muxer that will output the video as MKV.
+    Muxer* muxer = new Muxer("../videos/output_H264.mp4");  // good result
     // create the different components that make this come together
     try
     {
@@ -54,27 +74,27 @@ void record_MKV()
         AVRational frameRate = { 24, 1 };
 
         // All seem to work
-        //AVPixelFormat input_pix_fmt= AV_PIX_FMT_NV12;
         //AVPixelFormat outputPixFormat= AV_PIX_FMT_RGBA;
-        AVPixelFormat outputPixFormat= AV_PIX_FMT_YUV420P;
+        //AVPixelFormat outputPixFormat= AV_PIX_FMT_YUV420P;
+        AVPixelFormat outputPixFormat= AV_PIX_FMT_NV12;
 
         VideoEncoder * videoEncoder = new VideoEncoder(vcodec, muxer, frameRate, outputPixFormat);
+        AudioEncoder * audioEncoder = new AudioEncoder(audioCodec, muxer);
 
-        Demuxer * demuxer = new Demuxer(videoDevice, width, height, fps);
+        Demuxer * audioFile = new Demuxer(audioDevice, audioDeviceFormat, audioSampleRate, audioChannels, audioEncoder);
+        Demuxer * demuxer  = new Demuxer(videoDevice, width, height, fps);
+
+        audioFile->DecodeBestAudioStream(audioEncoder);
         demuxer->DecodeBestVideoStream(videoEncoder);
+
+        audioFile->PreparePipeline();
         demuxer->PreparePipeline();
 
-        int frameNumber = 0;
-
-        while (!demuxer->IsDone() && (frameNumber < 100))
+        while (!demuxer->IsDone() && !audioFile->IsDone())
         {
-            frameNumber = demuxer->GetFrameCount(demuxer->getVideoStreamIndx());
-            std::cerr << "frame number : " << frameNumber << "\n";
-
-            if (frameNumber > 100)
-                demuxer->Stop();
-
+            //int frameNumber = demuxer->GetFrameCount(demuxer->getVideoStreamIndx());
             demuxer->Step();
+            audioFile->Step();
         }
 
         // close the muxer and save the file to disk
@@ -86,6 +106,8 @@ void record_MKV()
         if (videoEncoder != nullptr)
             delete videoEncoder;
 
+        if (audioEncoder != nullptr)
+            delete audioEncoder;
 
         delete muxer;
     }
@@ -98,9 +120,13 @@ void record_MKV()
 
 int main(void)
 {
-	record_MKV();
+    avdevice_register_all();
+//    avdevice_register_all();
+//    avformat_network_init();
 
-	std::cout << "Encoding complete!" << "\n";
+    record_MKV();
+
+    std::cout << "Encoding complete!" << "\n";
 
 	return 0;
 }
