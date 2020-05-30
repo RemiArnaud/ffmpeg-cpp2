23c81d8b1490f4342cf46b8f5e3520818fdf70f2
erba <eric.bachard@free.fr>
2020-05-30 15:15:31 +0200

    Record Audio works very well. Let's make it available

--- demos/webcam_to_video/webcam_stream_to_SOUND.cpp
+++ demos/webcam_to_video/webcam_stream_to_SOUND.cpp
@@ -19,7 +19,7 @@ void record_VP9()
 {
 
     // Create a muxer that will output the video as MKV.
-    Muxer* muxer = new Muxer("../videos/output_VP9.avi");
+    Muxer* muxer = new Muxer("../videos/output_VP9.mkv");
 
     // These are example video and audio sources used below.
     // const char* videoDevice = "/dev/video0";
@@ -33,82 +33,9 @@ void record_VP9()
 
     AudioCodec * audioCodec = new AudioCodec(AV_CODEC_ID_AAC);
 
-    // FIXME : should work, but maybe not the best moment ?
-    // audioCodec->Open(40000, AV_SAMPLE_FMT_FLTP, audioSampleRate);
-
-    /*
-        AAC profile should be : 
-        Name = "AAC_Profile_128k",
-        Bitrate = 128000,
-        Rate = 48000
-    */
-
-    //  OUTPUT CODEC, linked to the encoder ...
-    VP9Codec  * lcodec = new VP9Codec();
-
-    /*
-        -speed 6 	Speed 5 to 8 should be used for live / real-time encoding. Lower numbers (5 or 6) are higher quality but require more CPU power. Higher numbers (7 or 8) will be lower quality but more manageable for lower latency use cases and also for lower CPU power devices such as mobile.
-        -tile-columns 4 	Tiling splits the video into rectangular regions, which allows multi-threading for encoding and decoding. The number of tiles is always a power of two. 0 = 1 tile, 1 = 2, 2 = 4, 3 = 8, 4 = 16, 5 = 32.
-        -frame-parallel 1 	Enable parallel decodability features.
-        -threads 8 	Maximum number of threads to use.
-        -static-thresh 0 	Motion detection threshold.
-        -max-intra-rate 300 	Maximum i-Frame bitrate (pct)
-        -deadline realtime 	Alternative (legacy) version of -quality realtime
-        -lag-in-frames 0 	Maximum number of frames to lag
-        -qmin 4 -qmax 48 	Minimum and maximum values for the quantizer. The values here are merely a suggestion and adjusting this will help increase/decrease video quality at the expense of compression efficiency.
-        -row-mt 1 	Enable row-multithreading. Allows use of up to 2x thread as tile columns. 0 = off, 1 = on.
-        -error-resilient 1 	Enable error resiliency features.
-    */
-
-    lcodec->SetGenericOption("speed", "6");
-    lcodec->SetGenericOption("threads", "4");
-    lcodec->SetGenericOption("row-mt", "1");
-
-    lcodec->SetGenericOption("qmin", "12");
-    lcodec->SetGenericOption("qmax", "34");
-
-
-    // https://slhck.info/video/2017/03/01/rate-control.html
-    lcodec->SetGenericOption("vbv-maxrate", "1000");  // 1M
-    lcodec->SetGenericOption("vbv-bufsize", "2000"); // 2M 
-
-    lcodec->SetGenericOption("movflags", "faststart");
-
-    //lcodec->SetProfile("high10"); // baseline, main, high, high10, high422
-    //lcodec->SetTune("film");  // film animation grain stillimage psnr ssim fastdecode zerolatency
-    //vcodec->SetPreset("veryslow"); // fast, medium, slow slower, veryslow placebo
-    //lcodec->SetCrf(23);
-
-
-    // Configure the codec to not do compression, to use multiple CPU's and to go as fast as possible.
-    lcodec->SetLossless(false);
-    lcodec->SetCpuUsed(5);
-    //lcodec->SetDeadline("realtime"); // other case : good
-    lcodec->SetDeadline("good"); // other case : good
-    lcodec->SetGenericOption("low_power", "true");
-
-    //   0       <------   18  <-------  23 -----> 28 ------> 51
-    //  lossless       better                 worse          worst
-    lcodec->SetCrf(23);  // 28 is default for VP9, while 23 is prefered for H264
-
     // create the different components that make this come together
     try
     {
-//Video        int width  = 1280;
-//Video        int height = 720;
-//Video        int fps = 24;
-
-        // FIXME : timing is not precise, and probably wrong
-        //AVRational frameRate = { 30, 1 };
-//Video        AVRational frameRate = { 24, 1 };
-
-        // All seem to work
-        //AVPixelFormat input_pix_fmt= AV_PIX_FMT_NV12;
-        //AVPixelFormat outputPixFormat= AV_PIX_FMT_RGBA;
-//Video        AVPixelFormat outputPixFormat= AV_PIX_FMT_YUV420P;
-
-        // VideoEncoder *   videoEncoder = new VideoEncoder(lcodec, muxer, frameRate, outputPixFormat);
-
         AudioEncoder *   audioEncoder = new AudioEncoder(audioCodec, muxer);
 
         RawAudioFileSource* audioFile = new RawAudioFileSource( audioDevice,
@@ -116,27 +43,23 @@ void record_VP9()
                                                                 audioSampleRate,
                                                                 audioChannels,
                                                                 audioEncoder);
-
-        // audioFile->DecodeBestAudioStream(audioEncoder);   /// PROBABLY IMPORTANT : create 2 identical muxers, and sync
-        // Demuxer *           demuxer   = new Demuxer(videoDevice, width, height, fps);
-        // demuxer->DecodeBestVideoStream(videoEncoder);
-
         audioFile->PreparePipeline();
-        // demuxer->PreparePipeline();
 
-        auto start = std::chrono::system_clock::now();
+        // https://en.cppreference.com/w/cpp/chrono
+        auto start = std::chrono::steady_clock::now();
 
         while (!audioFile->IsDone())
         {
-            auto current_time = std::chrono::system_clock::now();
-            //int frameNumber = demuxer->GetFrameCount(demuxer->getVideoStreamIndx());
+            auto current_time = std::chrono::steady_clock::now();
 
-            auto elapsed = current_time - start;
-            std::cout << "elapsed.count() =  "<< elapsed.count() << '\n';
+            std::chrono::duration<double> elapsed_seconds = current_time - start;
+
+            std::cout << "elapsed time  :   "<< elapsed_seconds.count() << '\n';
+
 
             audioFile->Step();
 
-            if ((elapsed.count()) > (30e9)) // 30s. If 1min it gives 1440 video frames exactly at 24 fps
+            if ((elapsed_seconds.count()) > (30)) // 30s. If 1min it gives 1440 video frames exactly at 24 fps
                 audioFile->Stop();
         }
 
@@ -145,9 +68,6 @@ void record_VP9()
 
         if (lcodec != nullptr)
             delete lcodec;
-
-        // if (videoEncoder != nullptr)
-        //     delete videoEncoder;
 
         if (audioFile != nullptr)
             delete audioFile;
